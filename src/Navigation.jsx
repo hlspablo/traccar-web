b967284b8ac6766fc374cf645d2dec9e1583820e
Pablo Henrique <pabllobeg@gmail.com>
2025-03-13 08:49:23 -0300

    Remove unused pages and simplify navigation
    - Delete ChangeServerPage, EventPage, GeofencesPage, NetworkPage, PositionPage, and associated components to streamline the project.
    - Update Navigation component to reflect the removal of these pages.
    - Simplify BottomMenu by hardcoding the navigation value to "map" and removing unnecessary logic.

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -3,29 +3,18 @@ import {
   Route, Routes,
 } from 'react-router-dom';
 import MainPage from './main/MainPage';
-import PositionPage from './other/PositionPage';
-import NetworkPage from './other/NetworkPage';
-import GeofencesPage from './other/GeofencesPage';
-import EventPage from './other/EventPage';
 import App from './App';
 import LoginPage from './login/LoginPage';
 import RegisterPage from './login/RegisterPage';
 import ResetPasswordPage from './login/ResetPasswordPage';
-import ChangeServerPage from './login/ChangeServerPage';
 
 const Navigation = () => (
   <Routes>
     <Route path="/login" element={<LoginPage />} />
     <Route path="/register" element={<RegisterPage />} />
     <Route path="/reset-password" element={<ResetPasswordPage />} />
-    <Route path="/change-server" element={<ChangeServerPage />} />
     <Route path="/" element={<App />}>
       <Route index element={<MainPage />} />
-
-      <Route path="position/:id" element={<PositionPage />} />
-      <Route path="network/:positionId" element={<NetworkPage />} />
-      <Route path="event/:id" element={<EventPage />} />
-      <Route path="geofences" element={<GeofencesPage />} />
     </Route>
   </Routes>
 );
--- src/common/components/BottomMenu.jsx
+++ src/common/components/BottomMenu.jsx
@@ -1,46 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {
   Paper, BottomNavigation, BottomNavigationAction, Badge,
 } from '@mui/material';
-
 import MapIcon from '@mui/icons-material/Map';
-
 import { useTranslation } from './LocalizationProvider';
 
 const BottomMenu = () => {
   const navigate = useNavigate();
-  const location = useLocation();
   const t = useTranslation();
-
   const socket = useSelector((state) => state.session.socket);
-
-  const currentSelection = () => {
-    if (location.pathname.startsWith('/reports')) {
-      return 'reports';
-    } if (location.pathname === '/') {
-      return 'map';
-    }
-    return null;
-  };
-
-  const handleSelection = (event, value) => {
-    switch (value) {
-      case 'map':
-        navigate('/');
-        break;
-      case 'reports':
-        navigate('/reports/combined');
-        break;
-      default:
-        break;
-    }
-  };
 
   return (
     <Paper square elevation={3}>
-      <BottomNavigation value={currentSelection()} onChange={handleSelection} showLabels>
+      <BottomNavigation value="map" onChange={() => navigate('/')} showLabels>
         <BottomNavigationAction
           label={t('mapTitle')}
           icon={(

e623c9b9878b829f4c9a3a454b884f1a6e71db22
Pablo Henrique <pabllobeg@gmail.com>
2025-03-13 18:53:27 -0300

    force ws socket connection

--- src/SocketController.jsx
+++ src/SocketController.jsx
@@ -33,7 +33,8 @@ const SocketController = () => {
   const features = useFeatures();
 
   const connectSocket = () => {
-    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    // const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const protocol = 'ws:';
     const socket = new WebSocket(`${protocol}//${window.location.host}/api/socket`);
     socketRef.current = socket;
 

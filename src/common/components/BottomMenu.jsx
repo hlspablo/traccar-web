acb44acf4116b9099f578d56658efc9dcd8639b1
Pablo Henrique <pabllobeg@gmail.com>
2025-03-14 14:47:42 -0300

    add logout button

--- src/common/components/BottomMenu.jsx
+++ src/common/components/BottomMenu.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import {
   Paper, BottomNavigation, BottomNavigationAction, Badge,
 } from '@mui/material';
 import MapIcon from '@mui/icons-material/Map';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { useTranslation } from './LocalizationProvider';
+import { sessionActions } from '../../store';
 
 const BottomMenu = () => {
   const navigate = useNavigate();
   const t = useTranslation();
   const socket = useSelector((state) => state.session.socket);
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    await fetch('/api/session', { method: 'DELETE' });
+    dispatch(sessionActions.updateUser(null));
+    navigate('/login');
+  };
 
   return (
     <Paper square elevation={3}>
@@ -24,7 +33,18 @@ const BottomMenu = () => {
           )}
           value="map"
         />
+        <BottomNavigationAction
+          onClick={handleLogout}
+          label={t('loginLogout')}
+          icon={(
+            <Badge color="error" variant="dot" overlap="circular" invisible={socket !== false}>
+              <LogoutIcon />
+            </Badge>
+          )}
+          value="logout"
+        />
       </BottomNavigation>
+
     </Paper>
   );
 };

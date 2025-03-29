3053e7d17d1148524fdfcdc8e85d177bfe903bc8
Pablo Henrique <pabllobeg@gmail.com>
2025-03-28 21:02:58 -0300

    Add Asaas proxy route to Vite configuration for API integration; enhance maintainability of proxy settings.

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(() => ({
     proxy: {
       '/api/socket': 'ws://coragemserver.top',
       '/api': 'http://coragemserver.top',
+      '/asaas-proxy': 'https://api-sandbox.asaas.com',
     },
   },
   build: {
